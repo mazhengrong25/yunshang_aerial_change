806a35a2de0c44510b367ba724a4aadc109450f7
wishQAQ <wishtime1024@gmail.com>
2020-11-04 00:03:14 +0800

    api对接

--- src/views/resolved.vue
+++ src/views/resolved.vue
@@ -8,7 +8,7 @@
             style="width: 160px"
             placeholder="请选择"
             allowClear
-            v-model="filter_from.Source"
+            v-model="filter_from.msgSource"
           >
             <a-select-option
               v-for="(item, index) in dictonaryList"
@@ -24,7 +24,7 @@
         <div class="list_title">具体来源</div>
         <div class="list_selece">
           <a-input
-            v-model="filter_from.Sign"
+            v-model="filter_from.sign"
             allowClear
             placeholder="手机号/邮箱"
           />
@@ -37,7 +37,7 @@
     </div>
 
     <div class="table_tool">
-      <a-button>批量停用</a-button>
+      <a-button @click="openParseFlagModal">无需解析</a-button>
     </div>
 
     <div class="table_main">
@@ -54,7 +54,7 @@
       >
         <a-table-column title="操作">
           <template slot-scope="record">
-            <a-tag  color="#FB9826" @click="dataParsing(record)">解析</a-tag>
+            <a-tag color="#FB9826" @click="dataParsing(record)">解析</a-tag>
           </template>
         </a-table-column>
         <a-table-column
@@ -68,15 +68,19 @@
           title="来源渠道"
           data-index="sourceName"
         ></a-table-column>
+        <a-table-column title="入库时间">
+          <template slot-scope="text, record">
+            {{
+              record.addTime
+                ? $moment(record.addTime).format("YYYY-MM-DD HH:mm:ss")
+                : ""
+            }}
+          </template>
+        </a-table-column>
         <a-table-column
-          key="upAddTime"
-          title="入库时间"
-          data-index="upAddTime"
-        ></a-table-column>
-        <a-table-column
-          key="profitCenterCode"
+          key="affiliationName"
           title="所属航司"
-          data-index="profitCenterCode"
+          data-index="affiliationName"
         ></a-table-column>
         <a-table-column
           key="pnr"
@@ -88,11 +92,25 @@
           title="外部订单号"
           data-index="correlativeOID"
         ></a-table-column>
-        <a-table-column
-          key="ParseFlag"
-          title="解析状态"
-          data-index="ParseFlag"
-        ></a-table-column>
+        <a-table-column title="解析状态">
+          <template slot-scope="text, record">
+            {{
+              record.parseFlag === 0
+                ? "待解析"
+                : record.parseFlag === 1
+                ? "自动解析成功"
+                : record.parseFlag === 2
+                ? "人工解析成功"
+                : record.parseFlag === -1
+                ? "解析失败"
+                : record.parseFlag === -2
+                ? "无关联订单"
+                : record.parseFlag === -3
+                ? "无需解析"
+                : record.parseFlag
+            }}
+          </template>
+        </a-table-column>
       </a-table>
       <div class="table_pagination">
         <a-pagination
@@ -110,195 +128,203 @@
 
     <!-- 解析 模态框 -->
     <a-modal
-      :title="parseTitle"
+      title="手工解析航变信息"
       :visible="parseVisible"
       :confirm-loading="confirmLoading"
       centered
       @ok="submitBtn"
       @cancel="cancelBtn"
-      width="1100px">
-      
-      <div class="parse_title"> 
-          <p>
-            【东海航空】尊敬的王文杰旅客，您预定的2020-10-21 15:30:00由郑州起飞到达深圳的DZ6220航班，
-            因流量控制原因导致航班时刻调整，目前起飞时间预计为2020-10-21北京时间16:30:00。请您按照新的起飞时间国内航班提前120分钟，
-            国际航班提前180分钟到达机场办理登机手续，东海航空祝您旅途愉快。
-          </p>
+      width="1100px"
+    >
+      <div class="parse_title">
+        <p>
+          {{flightForm.messageText}}
+        </p>
       </div>
       <div class="parse_middle">
-          <div class="parse_item">航变来源</div>
-          <div class="parse_select">
-              <a-select
-                placeholder="请输入"
-                style="width: 432px"
-                v-model="flight_source"
-              >
-                <a-select-option value="0">手工录入</a-select-option>
-                <a-select-option value="1">手工录入</a-select-option>
-              </a-select>
-          </div>
-          <div class="parse_item">所属部门</div>
-          <div class="parse_select">
-              <a-select
-                placeholder="请输入"
-                style="width: 432px"
-               v-model="flight_depart"
-              >
-                <a-select-option value="0">畅游国内</a-select-option>
-                <a-select-option value="1">畅游国内</a-select-option>
-              </a-select>
-          </div>
+        <div class="parse_item">航变来源</div>
+        <div class="parse_select">
+          <a-select
+            placeholder="请输入"
+            style="width: 432px"
+            v-model="flightForm.msgSource"
+          >
+            <a-select-option value="0">手工录入</a-select-option>
+            <a-select-option value="1">手工录入</a-select-option>
+          </a-select>
+        </div>
+        <div class="parse_item">所属部门</div>
+        <div class="parse_select">
+          <a-select
+            placeholder="请输入"
+            style="width: 432px"
+            v-model="flightForm.affiliationDept"
+          >
+            <a-select-option value="0">畅游国内</a-select-option>
+            <a-select-option value="1">畅游国内</a-select-option>
+          </a-select>
+        </div>
       </div>
 
       <div class="parse_seg">
-
         <div class="flight_level">
           <div class="flight_name">航空公司</div>
           <div class="flight_name_type">
-              <a-input  style="width:168px" placeholder="航司二字码" />
+            <a-input style="width: 168px" v-model="flightForm.airlineCode" placeholder="航司二字码" />
           </div>
         </div>
-
 
         <div class="flight_level">
           <div class="flight_name">原出发地</div>
           <div class="flight_name_type">
-              <a-input  style="width:168px" placeholder="例：重庆" />
+            <a-input style="width: 168px" v-model="flightForm.oFromCity" placeholder="例：重庆" />
           </div>
         </div>
 
         <div class="flight_level">
           <div class="flight_name">原目的地</div>
           <div class="flight_name_type">
-              <a-input style="width:168px" placeholder="例：重庆" />
+            <a-input style="width: 168px" v-model="flightForm.oToCity" placeholder="例：重庆" />
           </div>
         </div>
 
         <div class="flight_level">
           <div class="flight_name">新出发地</div>
           <div class="flight_name_type">
-              <a-input style="width:168px" placeholder="例：重庆" />
+            <a-input style="width: 168px" v-model="flightForm.fromCity" placeholder="例：重庆" />
           </div>
         </div>
 
         <div class="flight_level">
           <div class="flight_name">新目的地</div>
           <div class="flight_name_type">
-              <a-input style="width:168px" placeholder="例：重庆" />
+            <a-input style="width: 168px" v-model="flightForm.toCity" placeholder="例：重庆" />
           </div>
         </div>
 
         <div class="flight_level">
           <div class="flight_name">原出发地码</div>
           <div class="flight_name_type">
-              <a-input style="width:168px" placeholder="原出发地三字码" />
+            <a-input style="width: 168px" v-model="flightForm.oFromCityCode" placeholder="原出发地三字码" />
           </div>
         </div>
 
         <div class="flight_level">
           <div class="flight_name">原目的地码</div>
           <div class="flight_name_type">
-              <a-input style="width:168px" placeholder="原目的地三字码" />
+            <a-input style="width: 168px" v-model="flightForm.oToCityCode" placeholder="原目的地三字码" />
           </div>
         </div>
 
         <div class="flight_level">
           <div class="flight_name">新出发地码</div>
           <div class="flight_name_type">
-              <a-input style="width:168px" placeholder="新出发地三字码" />
+            <a-input style="width: 168px" v-model="flightForm.fromCityCode" placeholder="新出发地三字码" />
           </div>
         </div>
 
         <div class="flight_level">
           <div class="flight_name">新目的地码</div>
           <div class="flight_name_type">
-              <a-input style="width:168px" placeholder="新目的地三字码" />
+            <a-input style="width: 168px" v-model="flightForm.toCityCode" placeholder="新目的地三字码" />
           </div>
         </div>
 
         <div class="flight_level">
           <div class="flight_name">航变类型</div>
           <div class="flight_name_type">
-              <a-select
-                placeholder="请选择"
-                style="width: 168px"
-                v-model="flight_type"
-              >
-                <a-select-option value="0">航变取消</a-select-option>
-                <a-select-option value="1">航变取消</a-select-option>
-              </a-select>
+            <a-select
+              placeholder="请选择"
+              style="width: 168px"
+              v-model="flight_type"
+            >
+              <a-select-option value="0">航变取消</a-select-option>
+              <a-select-option value="1">航变取消</a-select-option>
+            </a-select>
           </div>
         </div>
 
-         <div class="flight_level">
+        <div class="flight_level">
           <div class="flight_name">子类型</div>
           <div class="flight_name_type">
-             <a-select
-                placeholder="请选择"
-                style="width: 168px"
-                v-model="child_type"
-              >
-                <a-select-option value="0">航班取消无保护</a-select-option>
-                <a-select-option value="1">航班取消无保护</a-select-option>
-              </a-select>
+            <a-select
+              placeholder="请选择"
+              style="width: 168px"
+              v-model="child_type"
+            >
+              <a-select-option value="0">航班取消无保护</a-select-option>
+              <a-select-option value="1">航班取消无保护</a-select-option>
+            </a-select>
           </div>
         </div>
 
         <div class="flight_level">
           <div class="flight_name">原航班号</div>
           <div class="flight_name_type">
-              <a-input style="width:168px" placeholder="请输入" />
+            <a-input style="width: 168px" placeholder="请输入" />
           </div>
         </div>
 
         <div class="flight_level">
           <div class="flight_name">新航班号</div>
           <div class="flight_name_type">
-              <a-input style="width:168px" placeholder="请输入" />
+            <a-input style="width: 168px" placeholder="请输入" />
           </div>
         </div>
 
         <div class="flight_level">
           <div class="flight_name">新目的地</div>
           <div class="flight_name_type">
-              <a-input style="width:168px" placeholder="例：重庆" />
+            <a-input style="width: 168px" placeholder="例：重庆" />
           </div>
         </div>
 
         <div class="flight_level">
           <div class="flight_name">原起飞时间</div>
           <div class="flight_name_type">
-               <a-date-picker style="width:168px" placeholder="起飞时间" />
+            <a-date-picker style="width: 168px" placeholder="起飞时间" />
           </div>
         </div>
 
         <div class="flight_level">
           <div class="flight_name">原到达时间</div>
           <div class="flight_name_type">
-              <a-date-picker style="width:168px" placeholder="到达时间" />
+            <a-date-picker style="width: 168px" placeholder="到达时间" />
           </div>
         </div>
 
         <div class="flight_level">
           <div class="flight_name">新起飞时间</div>
           <div class="flight_name_type">
-              <a-date-picker style="width:168px" placeholder="起飞时间" />
+            <a-date-picker style="width: 168px" placeholder="起飞时间" />
           </div>
         </div>
 
         <div class="flight_level">
           <div class="flight_name">新到达时间</div>
           <div class="flight_name_type">
-              <a-date-picker style="width:168px" placeholder="到达时间"/>
+            <a-date-picker style="width: 168px" placeholder="到达时间" />
           </div>
         </div>
-
-
-
       </div>
 
       <div class="parse_add" @click="addSegment()">再加一段</div>
-  
+    </a-modal>
+
+    <!-- 无需解析-->
+    <a-modal
+      title="提示"
+      :visible="parseFlagModal"
+      centered
+      :width="440"
+      :confirm-loading="parseFlagLoading"
+      @ok="parseFlagSubmit"
+      @cancel="parseFlagModal = false"
+    >
+      <div class="parse_flag_main">
+        <div>是否确定将选择的航变信息设置为无需解析？</div>
+        <p>一旦设置成功，该航变信息将不再关联显示！</p>
+      </div>
     </a-modal>
   </div>
 </template>
@@ -320,13 +346,15 @@ export default {
 
       parseVisible: false, // 新增/编辑弹窗
 
-      parseTitle:'手工解析航变信息',
+      parseTitle: "",
       confirmLoading: false, // 确定按钮加载动画
-      flight_source:'',  // 航变来源
-      flight_depart:'' , // 所属来源
-      flight_type:'',  //航变类型 
-      child_type:'', //子类型
+      flightForm: {}, // 解析数据
 
+
+      profitCenterList: [], // 利润中心列表
+
+      parseFlagModal: false, // 无需解析弹窗
+      parseFlagLoading: false, // 无需解析按钮状态
     };
   },
   methods: {
@@ -335,23 +363,16 @@ export default {
       this.$axios.get("api/token/authenticate").then((res) => {
         this.$axios.defaults.headers.Authorization = "Bearer " + res.data.token;
         this.getdata();
+        this.getDictonaryList();
       });
     },
 
     // 获取列表数据
     getdata() {
       let data = {
-        pageNo: 1,
-        pageSize: 10,
-        queryInfo: {
-          profitCenterCode: "",
-          msgSource: "",
-          ParseFlag: "",
-          messageText: "",
-          upAddTime: "",
-          downAddTime: "",
-          sign: "",
-        },
+        pageNo: this.current,
+        pageSize: this.currentSize,
+        queryInfo: this.filter_from,
       };
       this.$axios.post("/api/Origin/GetPage", data).then((res) => {
         if (res.data.isSuccess) {
@@ -359,15 +380,19 @@ export default {
           this.current = res.data.value.pageNo;
           this.currentTotal = res.data.value.totalCount;
 
-          this.dataList.forEach((item) => {
-            this.dictonaryList.forEach((oitem) => {
-              if (item.source === oitem.value) {
-                console.log(oitem);
-                item["sourceName"] = oitem.name;
-              }
+          if (this.dictonaryList.length > 0) {
+            this.dataList.forEach((item) => {
+              this.dictonaryList.forEach((oitem) => {
+                if (item.msgSource === oitem.value) {
+                  console.log(oitem);
+                  item["sourceName"] = oitem.name;
+                }
+              });
             });
-          });
+          }
+
           this.$forceUpdate();
+          console.log(this.dataList);
         }
       });
     },
@@ -385,7 +410,7 @@ export default {
             this.dictonaryList = res.data.value;
             this.dataList.forEach((item) => {
               this.dictonaryList.forEach((oitem) => {
-                if (item.source === oitem.value) {
+                if (item.msgSource === oitem.value) {
                   console.log(oitem);
                   item["sourceName"] = oitem.name;
                 }
@@ -401,13 +426,37 @@ export default {
       this.selectedRowKeys = val;
     },
 
+    // 无需解析弹窗
+    openParseFlagModal() {
+      if (this.selectedRowKeys.length < 1) {
+        return this.$message.warning("请至少选择一条数据");
+      }
+      this.parseFlagModal = true;
+    },
+    // 无需解析提交数据
+    parseFlagSubmit() {
+      this.parseFlagLoading = true
+      let data = {
+        type: "batch",
+        originIds: this.selectedRowKeys,
+      };
+      this.$axios.post('api/Origin/UpdateParseFlag',data)
+        .then(res =>{
+          this.parseFlagLoading = false
+          if(res.data.isSuccess){
+            this.$message.success(res.data.msg)
+            this.parseFlagModal = false;
+            this.getdata()
+          }
+        })
+    },
+
     // 解析数据
     dataParsing(val) {
       this.parseVisible = true;
-
     },
 
-      // 弹窗提交按钮
+    // 弹窗提交按钮
     submitBtn() {
       this.confirmLoading = true;
       setTimeout(() => {
@@ -419,7 +468,6 @@ export default {
     // 弹窗关闭按钮
     cancelBtn() {
       this.parseVisible = false;
-    
     },
 
     // 跳转页面
@@ -517,35 +565,32 @@ export default {
 
 .parse_title {
   display: flex;
-  height: 72px;  
+  height: 72px;
   border-radius: 2px;
-  border: 1px solid #D9E1EA;
-  background: #F1F3F5;
-  .p{
-    
-      font-size: 12px;
-      font-weight: 400;
-      color: #333333;
+  border: 1px solid #d9e1ea;
+  background: #f1f3f5;
+  p {
+    font-size: 12px;
+    font-weight: 400;
+    color: #333333;
   }
 }
 .parse_middle {
   display: flex;
-  margin:25px 25px;
+  margin: 25px 25px;
   .parse_select {
-    margin-right: 13px
+    margin-right: 13px;
   }
 }
 .parse_seg {
-
-  height: 304px; 
+  height: 304px;
   border-radius: 2px;
-  background: #F9F9F9;
+  background: #f9f9f9;
 
   .flight_level {
     display: inline-flex;
     align-items: center;
     .flight_name {
-
       font-size: 13px;
       color: #333333;
       font-weight: 400;
@@ -554,19 +599,33 @@ export default {
       text-align: right;
     }
     .flight_name_type {
-        font-weight: bold;
-        padding-left: 5px;
+      font-weight: bold;
+      padding-left: 5px;
     }
   }
-}&:not(:last-child) {
-      margin-bottom: 15px;
 }
 .parse_add {
-  
-  color: #0070E2;
+  color: #0070e2;
   font-weight: bold;
   font-size: 14px;
   cursor: pointer;
 }
 
+.parse_flag_main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  div {
+    font-size: 16px;
+    font-weight: 400;
+    color: #333333;
+    margin-bottom: 18px;
+  }
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    color: #ff0000;
+  }
+}
 </style>
